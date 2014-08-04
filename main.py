c1d8aa4a2770bdefe8d2b174af6fc7aa319b27b6
Jose Varela <joservarelaf@gmail.com>
2014-08-03 23:05:39 -0500

    get user items WIP

--- main.py
+++ main.py
@@ -20,23 +20,60 @@ import analyzer
 from google.appengine.ext import db
 
 class User(db.Model):
-    username = db.StringProperty (required = True)
-    pocket_access_token = db.StringProperty (required = True)
+    username = db.StringProperty(required=True)
+    pocket_access_token = db.StringProperty(required=True)
+
+    def save_bookmarks(self):
+        """Fetch items and store in db."""
+        new_bookmarks = []
+        pocket_r = pocket_connect.get_pocket_items(self.pocket_access_token, state='all', detailType='complete', count=10)
+        for b in pocket_r:
+            attrs = {
+                    'user': self,
+                    'title': b['resolved_title'],
+                    'has_been_read': b['status'] == '1',
+                    'is_favorite': b['favorite'] == '1',
+                    'url': b['resolved_url'],
+                    #'tags': str(b['tags'].keys()),
+                    'excerpt': b['excerpt'],
+                    'word_count': int(b['word_count'])
+            }
+            new_b = Bookmark(**attrs)
+            new_b.put()
+            new_bookmarks.append(new_b)
+        return new_bookmarks
+
+class Bookmark(db.Model):
+    user = db.ReferenceProperty(User, collection_name='bookmarks')
+    url = db.StringProperty(required=True)
+    title = db.StringProperty()
+    has_been_read = db.BooleanProperty()
+    is_favorite = db.BooleanProperty()
+    excerpt = db.TextProperty()
+    tags = db.TextProperty()
+    word_count = db.IntegerProperty()
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-    	
-    	request_token = pocket_connect.get_request_token()
-    	redirect_uri = 'http://localhost:14080/access?request_token=%s' % request_token
-    	self.redirect(str('https://getpocket.com/auth/authorize?request_token=%s&redirect_uri=%s' % (request_token,redirect_uri)))
-        
+        request_token = pocket_connect.get_request_token()
+        redirect_uri = 'http://localhost:14080/access?request_token=%s' % request_token
+        self.redirect(str('https://getpocket.com/auth/authorize?request_token=%s&redirect_uri=%s' % (request_token,redirect_uri)))
+
 class AccessHandler(webapp2.RequestHandler):
-	def get(self):
-		request_token = self.request.get('request_token')
-		credentials = pocket_connect.get_credentials(request_token)
-		user = User(username=credentials['username'], pocket_access_token=credentials['access_token'])
-		user.put()
-		self.response.write(credentials)
+    def get(self):
+        request_token = self.request.get('request_token')
+        credentials = pocket_connect.get_credentials(request_token)
+        user = User(username=credentials['username'], pocket_access_token=credentials['access_token'])
+        user.put()
+        user.save_bookmarks()
+        self.redirect('/users?user_key='+str(user.key()))
+
+class UsersHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write(User.get(self.request.get('user_key')).bookmarks.fetch(1000))
+
 app = webapp2.WSGIApplication([
-    ('/', MainHandler),('/access', AccessHandler)
+    ('/', MainHandler),
+    ('/access', AccessHandler),
+    ('/users', UsersHandler)
 ], debug=True)
